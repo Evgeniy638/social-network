a93757f43618ab1b2e59524af5afc521b3a6dd70
Eugene <subbotinjon01@mail.ru>
2019-10-14 21:15:54 +0300

    Updates

---
+++ precache-manifest.3baf6853e58c92d3a15e201ed14bb839.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7ccb7ad74e26723e3a5302833e262c",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "89b57d4fff41c8251946",
+    "url": "/social-network/static/css/main.a91303c6.chunk.css"
+  },
+  {
+    "revision": "cba39f7a8e09725cda11",
+    "url": "/social-network/static/js/2.c9b8059e.chunk.js"
+  },
+  {
+    "revision": "89b57d4fff41c8251946",
+    "url": "/social-network/static/js/main.61f2d2ee.chunk.js"
+  },
+  {
+    "revision": "cd8926f0a4b82e4e81df",
+    "url": "/social-network/static/js/runtime~main.eb74f080.js"
+  },
+  {
+    "revision": "4b0cb4ba419a9a61d6efcae770d84bde",
+    "url": "/social-network/static/media/avatar.4b0cb4ba.png"
+  },
+  {
+    "revision": "02f9fbc4e0443bded0a11f6216fb17a8",
+    "url": "/social-network/static/media/colorful-loader-gif-transparent-13.02f9fbc4.gif"
+  }
+]);
